d995e4b3b39bff4cf72c92540608cd5d7b9a18cc
noor <noor.web.journey@gmail.com>
2019-06-09 19:55:59 +0600

    BLADE and without php tag

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,7 +5,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title><?php echo $title; ?></title>
+    <title>{{$title}}</title>
 </head>
 <body>
     <div class="row">
@@ -18,21 +18,22 @@
                     <th>DETAILS</th>
                     <th>ACTIONS</th>
                 </tr>
-                <?php foreach ($blogs as $blog){ ?>
+                <?php /*foreach ($blogs as $blog){*/ ?>
+                @foreach($blogs as $blog)
                     <tr>
-                        <td><?php echo $blog->id; ?></td>
-                        <td><?php echo $blog->title; ?></td>
-                        <td><?php echo $blog->details; ?></td>
+                        <td>{{$blog->id}}</td>
+                        <td>{{$blog->title}}</td>
+                        <td>{{$blog->details}}</td>
                         <td>
-                            <a style="float: left;" class="btn btn-info" href="<?php echo route('blog.edit',$blog->id); ?>">Edit</a>
-                            <form style="float: left;" action="<?php echo route('blog.destroy',$blog->id); ?>" method="post">
-                                <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+                            <a style="float: left;" class="btn btn-info" href="{{route('blog.edit',$blog->id)}}">Edit</a>
+                            <form style="float: left;" action="{{route('blog.destroy',$blog->id)}}" method="post">
+                                @csrf
                                 <input type="hidden" name="_method" value="DELETE">
                                 &nbsp;&nbsp; <button onclick="return confirm('are you sure !!')" class="btn btn-danger" type="submit">Delete</button>
                             </form>
                         </td>
                     </tr>
-                <?php } ?>
+                @endforeach
             </table>
         </div>
     </div>
